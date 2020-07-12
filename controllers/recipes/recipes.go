dfd8aee28464b0b8d4903c1ff254740b4f6978a6
Tri Vuong <trivektor@gmail.com>
2020-07-12 04:28:00 -0700

    Endpoint to create recipies

--- controllers/recipes/recipes.go
+++ controllers/recipes/recipes.go
@@ -2,12 +2,30 @@ package recipes
 
 import (
   "net/http"
+  "encoding/json"
+  "go-mongo-food/db"
+  "go.mongodb.org/mongo-driver/bson"
+  "go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type Recipe struct {
+  Name string
+  Description string
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("Recipes!\n"))
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
-  
+  var recipe Recipe
+
+  json.NewDecoder(r.Body).Decode(&recipe)
+
+  collection := db.Client.Database("food_recipes").Collection("recipes")
+  res, _ := collection.InsertOne(db.Context, bson.M{"name": recipe.Name, "description": recipe.Description})
+  id := res.InsertedID.(primitive.ObjectID)
+
+  // https://stackoverflow.com/a/49937678/477697
+  w.Write([]byte(id.Hex()))
 }
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+  "context"
+  "time"
   "net/http"
+  "os"
   "github.com/gorilla/mux"
-  "./controllers/recipes"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+  "go-mongo-food/controllers/recipes"
+  "go-mongo-food/db"
 )
 
 func main() {
+  db.Client, _ = mongo.NewClient(options.Client().ApplyURI(os.Getenv("CHATTYY_DB_CONNECTION_STRING")))
+  db.Context, db.CancelFunc = context.WithTimeout(context.Background(), 10*time.Second)
+  db.Client.Connect(db.Context)
+
   r := mux.NewRouter()
-  r.HandleFunc("/recipes", recipes.Index)
   r.HandleFunc("/recipes", recipes.Create).Methods("POST")
+  r.HandleFunc("/recipes", recipes.Index)
   http.ListenAndServe(":8080", r)
+
+  defer db.Client.Disconnect(db.Context)
+  defer db.CancelFunc()
 }
